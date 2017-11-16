48cadee32055aa6a7cc1d981a763871fbc932e29
Fredrik <Fredrik@DESKTOP-QPEG16D>
2017-11-16 03:16:06 +0100

    remove unused old stuff

--- src/prop/assignment0/Util.java
+++ src/prop/assignment0/Util.java
@@ -6,10 +6,5 @@ public class Util {
 	public static void appendTabs(StringBuilder builder, int tabs) {
 		for (int i = 0; i<tabs; i++) 
 			builder.append("\t");
-	}
-	
-	public static Object[] expandArray(Object[] arr) {
-		return Arrays.copyOf(arr, arr.length+2);
-	}
-	
+	}	
 }
